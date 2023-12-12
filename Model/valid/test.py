7c97139bd8ad954779b786523acb8ff6c97b1b38
Wu Tong <18805904201@163.com>
2023-12-12 23:30:17 +0800

    Update test.py, which still have problems.

--- Model/valid/test.py
+++ Model/valid/test.py
@@ -25,15 +25,16 @@ model = torch.load(model_save_dir, map_location=config.device)
 
 #####construct dataset
 DataSets = []
-csv_names = [csv_name[:-4] for csv_name in os.listdir('../../data/20231124')]
+csv_names = [csv_name[:-4] for csv_name in os.listdir('../../data/20231128')]
 for csv_name in csv_names:
-    DataSets.append(Stock_Dataset(data_dir='../../data/20231124', day_windows=config.day_windows+1,
+    DataSets.append(Stock_Dataset(data_dir='../../data/20231128', day_windows=config.day_windows+1,
                                   flag=csv_name, device=config.device))
 
 model.eval()
 PNL = 0
 TRADES = 0
 WINS = 0
+PNLS = np.array([])
 
 start = time.time()
 for i in tqdm(range(len(DataSets))):
@@ -52,67 +53,67 @@ for i in tqdm(range(len(DataSets))):
         THRESHOLDS = 0.01
         pnl = 0
 
-        if j != len(dataset) - 1:
-            with torch.no_grad():
-                preds = model(data[:-1].unsqueeze(0))[0]
+        with torch.no_grad():
+            preds = model(data[:-1].unsqueeze(0))[0]
 
-            preds_list.append(preds.cpu().detach().numpy())
+        preds_list.append(preds.cpu().detach().numpy())
 
-            preds_next_time = preds[0]
-            preds_next_period = preds[1]
-            # print("p-p", (preds_next_time-preds_next_period).tolist())
+        preds_next_time = preds[0]
+        preds_next_period = preds[1]
+        # print("p-p", (preds_next_time-preds_next_period).tolist())
 
-            ###################### OUR STRATEGY #######################
+        ###################### OUR STRATEGY #######################
 
-            cnt += 1
-            # print(data, cnt)
-            if last != 'sell' and \
-                    ((preds_next_time - preds_next_period > THRESHOLDS 
-                    and data[-2,0] * (1.0 + preds_next_time/100) > last_prices)
-                    or cnt >= MAXTIME):
-                if last == 'buy':
-                    TRADES += 1
-                cnt = 0
-                if last == None:
-                    last_prices = data[-1, 4]
-                    empty = 0
-                else:
-                    pnl = 2 * (data[-1, 4] - last_prices) / last_prices
-                    last_prices = data[-1, 4]
-                last = 'sell'
-                print(' sell, pnl=', pnl*1e4)
-            elif last != 'buy' and \
-                    ((preds_next_period - preds_next_time > THRESHOLDS 
-                    and data[-2,0] * (1.0 + preds_next_time/100) < last_prices)
-                    or cnt >= MAXTIME):
-                if last == 'sell':
-                    TRADES += 1
-                cnt = 0
-                if last == None:   # buy
-                    last_prices = data[-1, 4]
-                    empty = 0
-                else:
-                    pnl = 2 * (last_prices - data[-1, 4]) / last_prices
-                    last_prices = data[-1, 4]
-                last = 'buy'
-                print(' buy, pnl=', pnl*1e4)
+        cnt += 1
+        # print(data, cnt)
+        if last != 'sell' and \
+                ((preds_next_time - preds_next_period > THRESHOLDS 
+                and data[-2,0] * (1.0 + preds_next_time/100) > last_prices)
+                or cnt >= MAXTIME):
+            if last == 'buy':
+                TRADES += 1
+            cnt = 0
+            if last == None:
+                last_prices = data[-1, 4]
+            else:
+                pnl = (data[-1, 4] - last_prices) / last_prices
+                last_prices = data[-1, 4]
+            last = 'sell'
+            print(' sell, pnl=', pnl*1e4)
+        elif last != 'buy' and \
+                ((preds_next_period - preds_next_time > THRESHOLDS 
+                and data[-2,0] * (1.0 + preds_next_time/100) < last_prices)
+                or cnt >= MAXTIME):
+            if last == 'sell':
+                TRADES += 1
+            cnt = 0
+            if last == None:   # buy
+                last_prices = data[-1, 4]
+            else:
+                pnl = (last_prices - data[-1, 4]) / last_prices
+                last_prices = data[-1, 4]
+            last = 'buy'
+            # print(' buy, pnl=', pnl*1e4)
         ###################### END STRATEGY #######################
-        else:   # 强制平仓
+        if j == len(dataset)-1:   # 强制平仓
             if last == 'buy':
                 TRADES += 1
                 # 强制卖出
                 pnl = (data[-1, 4] - last_prices) / last_prices
-                print('force: sell, pnl=', pnl*1e4)
+                # print('force: sell, pnl=', pnl*1e4)
             elif last == 'sell':
                 TRADES += 1
                 # 强制买入
                 pnl = (last_prices - data[-1, 4]) / last_prices
-                print('force: buy, pnl=', pnl*1e4)
-            
+                # print('force: buy, pnl=', pnl*1e4)
+
+        if pnl != 0:
+            PNL += pnl    
+            PNLS = np.append(PNLS, pnl.cpu().detach().numpy())
+        # print(PNLS)
         if pnl > 0:
             WINS += 1
-            PNL += pnl
-
+            
     if (i+1) % 10 == 0:
         print(f'{i} / {len(csv_names)}, pnl={PNL * 1e4/TRADES:.4f}, win={WINS / TRADES:.4f}, trade={TRADES}')
 
@@ -128,3 +129,6 @@ print('  pnl:', PNL)
 print('  win rate:', WIN_RATE)
 print('  trades:', TRADES)
 print('score:', SCORE)
+print('pnl_avg:', PNLS.mean())
+print('pnl_std:', PNLS.std())
+print('sharpe_ratio:', PNLS.mean() / PNLS.std())
